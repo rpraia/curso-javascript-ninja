c2442806fdbf70a6e97050091ab623dc9abf33f8
rpraia <66320933+rpraia@users.noreply.github.com>
2020-06-09 14:12:27 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// var varArray = [ 'copo', betaFunction(), true, arvore.folhas, 666 ];
+var varArray = [ 'copo', betaFunction(), true, arvore.folhas, 666 ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
@@ -14,7 +14,7 @@ console.log( arrFunc( japan ) );
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-arrFunc( japan )[1]
+console.log( arrFunc( japan )[1] ); // moti
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -26,7 +26,7 @@ segundo parâmetro.
 function numFunc( x, y ) {
   return x[y];
 }
-numFumc( [ 6, 66, 666 ], 1 ) // 66
+console.log( numFumc( [ 6, 66, 666 ], 1 ) ); // 66
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
@@ -37,7 +37,11 @@ var arrNum = [ 100, 200, 300, 400, 500 ];
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log( numFumc( arrNum, 0 ) ); // 100
+console.log( numFumc( arrNum, 1 ) ); // 200
+console.log( numFumc( arrNum, 2 ) ); // 300
+console.log( numFumc( arrNum, 3 ) ); // 400
+console.log( numFumc( arrNum, 4 ) ); // 500
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -53,30 +57,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book( name ) {
+  var stock = {
+    'Admiravel mundo novo': { 
+      quantidadePaginas: 345, 
+      autor: 'Autor 1', 
+      editora: 'Padrão' 
+    },
+    'A Revolução dos bichos': { 
+      quantidadePaginas: 260, 
+      autor: 'Autor 2', 
+      editora: 'pubLivros' 
+    },
+    'A Ilha': { 
+      quantidadePaginas: 430, 
+      autor: 'Autor 3', 
+      editora: 'E-livros' 
+    }
+  };
+  
+  return !name ? stock : stock[ name ];  
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log( book() );
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'Admiravel mundo novo tem';
+console.log( 'O livro ' + bookName + ' ' + book( 'Admiravel mundo novo' ).quantidadePaginas + ' páginas!' )
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O autor do livro ' + bookName + ' é ' + book( 'Admiravel mundo novo' ).autor )
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro ' + bookName + ' foi publicado pela editora ' + book( 'Admiravel mundo novo' ).editora )
