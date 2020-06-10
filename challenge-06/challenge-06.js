a494970d8c63e8e7fe7fc2ae928c917c6a88dc19
rpraia <66320933+rpraia@users.noreply.github.com>
2020-06-10 15:40:50 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -1,10 +1,10 @@
 /*
-Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer
+Vamos falar um pouco sobre "Futebol 'Americano'". Escolha um campeonato estadual qualquer
 para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'NFL';
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +12,13 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = [ 
+  'Green Bay Packers', 
+  'New Orleans Saints', 
+  'Seattle Seahawks', 
+  'Pittsburgh Steelers', 
+  'Las Vegas Raiders'
+]
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +38,43 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+function showTeamPosition( position ) {
+
+  if( position < 1 || position > 5 ) {
+    return 'Não temos a informação do time que está nessa posição.';
+  }
+
+  return 'O time que está em ' + position + 'º lugar é o ' + teams[ position - 1 ] + '.';
+
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log( showTeamPosition( 1 ) ); // O time que está em 1º lugar é o Green Bay Packers.
+console.log( showTeamPosition( 2 ) ); // O time que está em 2º lugar é o New Orleans Saints.
+console.log( showTeamPosition( 4 ) ); // O time que está em 3º lugar é o Pittsburgh Steelers.
+console.log( showTeamPosition( 5 ) ); // O time que está em 5º lugar é o Las Vegas Raiders.
+
+console.log( showTeamPosition( 8 ) ); //  Não temos a informação do time que está nessa posição.
+
+
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+var numStart = 20;
+
+while( numStart <= 30 ) {
+
+    console.log( numStart++ );
+
+}
+
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +88,48 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+
+function convertToHex( color ) {
+
+  switch( color ) {
+
+    case 'black': 
+      hexa = '#000000';
+    break;
+
+    case 'white': 
+      hexa = '#ffffff';
+    break;
+
+    case 'light grey': 
+      hexa = '#999999';
+    break;
+
+    case 'mediun grey': 
+      hexa = '#666666';
+    break;
+
+    case 'dark grey': 
+      hexa = '#333333';
+    break;
+
+    default: 
+      return 'Não temos o equivalente hexadecimal para ' + color + '.';
+
+  }
+
+  return 'O hexadecimal para a cor ' + color + ' é ' + hexa + '.'
+
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log( convertToHex( 'black' ) );
+console.log( convertToHex( 'white' ) );
+console.log( convertToHex( 'light grey' ) );
+console.log( convertToHex( 'mediun grey' ) );
+console.log( convertToHex( 'dark grey' ) );
+console.log( convertToHex( 'blue' ) );
+console.log( convertToHex( 'green' ) );
+console.log( convertToHex( 'yellow' ) );
